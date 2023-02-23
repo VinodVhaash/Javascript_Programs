6a30af809af847805c925c63fdf2076d0aefee03
Vinod Patil <113292128+VinodVhaash@users.noreply.github.com>
2023-02-23 19:18:04 +0000

    Added few programs

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function isPrime(numb) {
   }
 }
 
+console.log("------------------------------");
 
 
 //1)Write a program to print the first 10 numbers of the Fibonacci sequence.
@@ -41,6 +42,7 @@ function FibonacciFun(number) {
 
   }
 }
+console.log("------------------------------");
 
 //Write a program to sort an array of objects by a specific property.
 const persons = [
@@ -64,6 +66,7 @@ const persons = [
 
 const SortPerson = persons.sort((a, b) => { return a.Age - b.Age; });
 console.log(SortPerson);
+console.log("------------------------------");
 
 
 // console.log(persons);
@@ -79,6 +82,7 @@ const arrayI = new Set(arraI);
 console.log(arrayI);
 
 
+console.log("------------------------------");
 
 
 //Sort a array element in array and show in reverse.
@@ -88,6 +92,7 @@ let arr = [10, 56, 23, 47, 96, 11];
 let sortedArr = arr.sort((x, y) => x - y);
 let reverseArr = sortedArr.reverse();
 console.log(reverseArr);
+console.log("------------------------------");
 
 //3)Write a program to find the largest element in an array.
 let arr1 = [10, 56, 23, 47, 96, 11];
@@ -96,6 +101,7 @@ let sortArr1 = arr1.sort((p, q) => p - q);
 let rev = sortArr1.reverse();
 
 console.log("Largest number in a array:" + rev[0]);
+console.log("------------------------------");
 
 //6)Write a program to find the sum of all numbers in an array.
 let arr6 = [10, 45, 6, 35, 41];
@@ -104,6 +110,7 @@ for (let i = 0; i < arr6.length; i++) {
   sum += arr6[i];
 }
 console.log("Sum of all Numbers in an array:" + sum);
+console.log("------------------------------");
 
 //4)Write a program to remove duplicates from an array in JavaScript.
 
@@ -111,6 +118,7 @@ const arr4 = [2, 5, 3, 5, 7, 2, 8];
 
 const arrs4 = new Set(arr4);
 console.log(arrs4);
+console.log("------------------------------");
 
 //5)Write a program to check if a string is a palindrome in JavaScript.
 const string1 = "Manisha" //prompt("Enter a string:");
@@ -129,6 +137,7 @@ function isPalindrome(string1) {
     }
   }
 }
+console.log("------------------------------");
 
 //2)Write a program to reverse a string in JavaScript.
 
@@ -144,6 +153,7 @@ function reverseSentence(sentence) {
   }
   console.log(reverseStr);
 }
+console.log("------------------------------");
 
 //9)Write a program to flatten a nested array.
 
@@ -155,8 +165,51 @@ const arr7 = [1, [2, [3, 4, [5, 6]]]];
 const arrv = arr7.flat(Infinity);
 
 console.log(arrv);
+console.log("------------------------------");
+
+//largest element in a array.
+
+const points = [11, 56, 89, 42, 35, 47];
+
+let maxNum = 0;
+
+for (let i = 0; i < points.length; i++) {
+  if (points[i] > maxNum) {
+    maxNum = points[i];
+  }
+}
+console.log(maxNum);
+console.log("------------------------------");
 
 
+//output : 65
+
+//Find duplicate elements in a array.
+const scores = [11, 56, 11, 47, 47, 22, 33, 65];
+var dupli = [];
+      console.log("Duplicate Elements are:");
+for (let i = 0; i < scores.length; i++) {
+  for (let j = i+1; j < scores.length; j++) {
+    if (scores[i] == scores[j]) {
+      console.log(scores[i]);
+
+    }
+  }
+}
+console.log("------------------------------");
+
+//Factorial of a number
+
+const a=prompt("Enter a number:");
+let fact=1;
+let factorial=FactorialofNumber(a);
+console.log(factorial);
+function FactorialofNumber(a){
+  for(let i=a;i>0;i--){
+    fact=fact*i;
+  }
+  return fact;
+}
 
 
 
